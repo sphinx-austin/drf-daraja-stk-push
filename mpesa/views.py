9dd12ffea710c1d1809c5130fd80eb3722df0139
sphinx-austin <austinjuniour4@gmail.com>
2023-10-23 06:15:07 +0300

    test view

--- mpesa/views.py
+++ mpesa/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    data = {
+        'id': 1,
+        'name': 'John',
+        'phonenumber': '12345678'
+
+    }
+    return render(request, 'index.html', {'data': data})
