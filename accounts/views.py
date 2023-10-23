4c2d01e5601978bbc703c2ff67a174abad48c1a8
sphinx-austin <austinjuniour4@gmail.com>
2023-10-23 06:13:36 +0300

    update

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-
-
-def index(request):
-    data = {
-        'id': 1,
-        'name': 'John',
-        'phonenumber': '12345678'
-
-    }
-    return render(request, {data: data})
